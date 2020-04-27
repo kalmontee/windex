370bd2ba06f166c1676fbb2113afd82aab36f3fa
Kelvin Almonte <kelvin.almonte95@gmail.com>
2020-04-27 19:55:47 -0400

    Organization

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -9,21 +9,21 @@ module.exports = {
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
-  
+
   findById(req, res) {
     db.Book
       .findById(req.params.id)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
-  
+
   create(req, res) {
     db.Book
       .create(req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
-  
+
   update(req, res) {
     db.Book
       .findOneAndUpdate({ _id: req.params.id }, req.body)
@@ -37,5 +37,5 @@ module.exports = {
       .then(dbModel => dbModel.remove())
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
-  }
+  },
 };
