6b3d1185c102fa7f9d3b47c1095f5c38346238ee
Kelvin Almonte <kelvin.almonte95@gmail.com>
2020-04-27 19:54:03 -0400

    Changed the googleId to 'id' and authors type from a String to an Array.

--- models/book.js
+++ models/book.js
@@ -2,8 +2,9 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
+  id: { type: String, require: true, unique: true },
   title: { type: String, required: true },
-  author: { type: String, required: true },
+  authors: { type: Array, required: true },
   description: String,
   image: { type: String, required: true },
   link: { type: String, required: true },
