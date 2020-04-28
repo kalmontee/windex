65f80d39ec010c01a0a2c30bf77534c1134607a8
Kelvin Almonte <kelvin.almonte95@gmail.com>
2020-04-28 17:13:12 -0400

    Setting up React-Router-DOM with adding a Switch component for all the Routes.

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,23 @@
-import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import NavBar from "./components/Navigation/NavigationItems/NavigationItems";
 import Jumbotron from './components/UI/Jumbotron/Jumbotron';
 import Search from "./pages/Search";
 import Saved from "./pages/Saved";
 
-function App() {
-  return (
-    <Router>
-      <>
+class App extends Component {
+  render() {
+    return (
+      <Router>
         <NavBar />
         <Jumbotron />
-        <Route exact path="/" component={Search} />
-        <Route exact path="/Saved" component={Saved} />
-      </>
-    </Router>
-  );
+        <Switch>
+          <Route exact path="/" component={Search} />
+          <Route exact path="/Saved" component={Saved} />
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 export default App;
