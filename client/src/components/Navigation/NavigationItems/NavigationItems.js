46348061d35b8b1e3e76ad56958e40a3f91763bb
Kelvin Almonte <kelvin.almonte95@gmail.com>
2020-04-28 17:14:35 -0400

    Modified the header.

--- client/src/components/Navigation/NavigationItems/NavigationItems.js
+++ client/src/components/Navigation/NavigationItems/NavigationItems.js
@@ -6,17 +6,14 @@ import classes from './NavigationItems.module.css';
 const NavBar = (props) => {
   return (
     <nav className={classes.Navbar}>
-      <ul>
+      <div>
         <h2>Google Books</h2>
-        <NavigationItem link="/" active>Search</NavigationItem>
-        <NavigationItem link="" >Saved</NavigationItem>
+      </div>
+      <ul>
+        <NavigationItem />
       </ul>
     </nav>
   )
 }
 
 export default NavBar;
-
-{/* <Link to="/saved" className={window.location.pathname === "/saved" ? ".NavLink .active" : classes.NavLink}>
-Saved
-</Link> */}
