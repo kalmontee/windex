f528e33bd873264f0130d1d0c1e62a083bfdb9ee
Kelvin Almonte <kelvin.almonte95@gmail.com>
2020-04-29 18:48:07 -0400

    Styled the NotFound page

--- client/src/pages/NotFound/NotFound.js
+++ client/src/pages/NotFound/NotFound.js
@@ -4,13 +4,17 @@ import classes from './NotFound.module.css';
 
 function notFound() {
   return (
-    <div className={classes.NotFound}>
-      <h1 className={classes.Emoji}>4<span role="img" aria-label="Face With Rolling Eyes Emoji">😔</span>4</h1>
-      
-      <h1 className={classes.Text}>
-      OOPS! PAGE NOT FOUND
+    <main className={classes.NotFoundPage}>
+      <div className={classes.Info}>
+        <h1 className={classes.Emoji}>4<span role="img" aria-label="Face With Rolling Eyes Emoji">😔</span>4</h1>
+
+        <h1 className={classes.Text}>
+          OOPS! PAGE NOT FOUND
       </h1>
-    </div>
+      </div>
+
+      <div className={classes.Background}></div>
+    </main>
   );
 }
 // 🙄
