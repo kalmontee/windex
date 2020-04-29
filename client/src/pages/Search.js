0b3d675856ba306ff1608c88819d030bbbfb2c0e
Kelvin Almonte <kelvin.almonte95@gmail.com>
2020-04-29 15:03:09 -0400

    Refractor the save button and added comments

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -3,13 +3,14 @@ import { Container, Row } from "../components/Grid";
 import BookDetails from '../components/Books/BookDetails/BookDetails';
 import BookList from '../components/Books/BookDetails/BookList/BookList';
 import InputSearch from '../components/InputSearch/InputSearch';
-// import Aux from '../HOC/Auxiliary';
+import Spinner from '../components/UI/Spinner/Spinner';
 import API from "../utils/API";
 
 class Search extends Component {
   state = {
     search: "",
     message: "",
+    loader: false,
     books: [],
   }
 
@@ -18,13 +19,19 @@ class Search extends Component {
 
   // Making a request to Google Books API
   searchGoogleBooks = (query) => {
+    // Initialize the loader
+    this.setState({ loader: true });
     API.search(query)
-      .then(res => this.setState({ books: res.data.items }))
-      // Throw in another component displaying an error message.
+      .then(res => this.setState({
+        books: res.data.items,
+        loader: false // remove the loader once it finds results
+      }))
+      // Throw in a Modal component displaying an error message.
       .catch(() =>
         this.setState({
+          loader: true, // Continue the loader is there's an error
           books: [],
-          message: "No New Books Found, Try a Different Query"
+          message: "No New Books Found, Try a Different Query" // Needs to be placed somewhere (could throw modal here)
         })
       );
   }
@@ -38,6 +45,7 @@ class Search extends Component {
   saveBookHandler = (id) => {
     let book = this.state.books.find(book => book.id === id);
 
+    // Saving all books contents to the database
     API.saveBook({
       id: book.id,
       title: book.volumeInfo.title,
@@ -46,8 +54,8 @@ class Search extends Component {
       description: book.volumeInfo.description,
       image: book.volumeInfo.imageLinks.thumbnail
     })
-      .then(this.setState({ message: alert("Your book is saved") }))
-      .catch(err => console.log(err))
+      .then(this.setState({ message: alert("Your book is saved") })) // Alerting the user the book has been saved.
+      .catch(err => console.log(err)) // Pop a modal saying there's an alert
   }
 
   render() {
@@ -58,21 +66,26 @@ class Search extends Component {
           {this.state.books.map(book => {
             return (
               <BookDetails
-                // Getting multiple books of the same IDs. If they have the same ID then give book.etag as a second ID.
-                key={book.id === book.id ? book.etag : null}
+                // Getting books with the same IDs. If they have the same ID then give book.etag as a second ID.
+                key={book.id ? book.etag : null}
                 authors={book.volumeInfo.authors.join(', ')}
                 title={book.volumeInfo.title}
                 synopsis={book.volumeInfo.description ? book.volumeInfo.description : null}
                 link={book.volumeInfo.infoLink}
                 // Sometimes the API does not have a imageLinks thumbnail images. Gives me undefined -- bug
                 thumbnail={book.volumeInfo.imageLinks.thumbnail ? book.volumeInfo.imageLinks.thumbnail : null}
-                SaveBookBtn={() => this.saveBookHandler(book.id)}
-              >
-              </BookDetails>
+                booksBtnHandler={() => this.saveBookHandler(book.id)}
+                name="Save Book"
+                btnType={"SaveBtn"}
+              />
             )
           })}
         </BookList>
       );
+    }
+
+    if (this.state.loader) {
+      booksResult = <Spinner />
     }
 
     return (
