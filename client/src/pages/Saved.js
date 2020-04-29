9ffcc882bca8b7aabbf1fd4cc8f752263c67e0c6
Kelvin Almonte <kelvin.almonte95@gmail.com>
2020-04-29 15:07:56 -0400

    Refractor the save button and added comments. Changed the component name to 'SavedBooks'

--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -1,15 +1,69 @@
 import React, { Component } from 'react';
-// import { link } from 'react-router-dom';
 
+import { Container } from '../components/Grid/index';
+import BookDetails from '../components/Books/BookDetails/BookDetails';
+import BookList from '../components/Books/BookDetails/BookList/BookList';
+import Spinner from '../components/UI/Spinner/Spinner';
+import API from '../utils/API';
 
-class Saved extends Component {
-    render() {
-        return (
-            <div>
+class SavedBooks extends Component {
+   state = {
+      allBooks: [],
+      loader: false,
+      error: ""
+   }
 
-            </div>
-        )
-    }
+   // componentDidAmount will automatically fetch data from the server
+   componentDidMount = () => {
+      // Initialize the loader
+      this.setState({ loader: true });
+      // Calling API.getBooks function to make a request to the server
+      API.getBooks()
+         .then(res => this.setState({ allBooks: res.data, loader: false }))
+         .catch(err => this.setState({ error: err }));
+   }
+
+   deleteBookHandler = (id) => {
+      API.deleteBook(id)
+         .then(this.setState({ err: alert("Successful!") }))
+         .then(res => this.componentDidMount())
+         .catch(err => console.log(err));
+   }
+
+   render() {
+      let booksResult = null;
+      if (this.state.allBooks.length > 0) {
+         booksResult = (
+            <BookList>
+               {this.state.allBooks.map(book => {
+                  return (
+                     <BookDetails
+                        key={book._id}
+                        authors={book.authors.join(', ')}
+                        title={book.title}
+                        synopsis={book.description}
+                        link={book.link}
+                        thumbnail={book.image}
+                        booksBtnHandler={() => this.deleteBookHandler(book._id)}
+                        name="Delete Book"
+                        btnType={"DeleteBtn"}
+                     />
+                  )
+               })}
+            </BookList>
+         );
+      }
+      
+      if (this.state.loader) {
+         booksResult = <Spinner />
+      }
+      
+      return (
+         <Container>
+            {booksResult}
+         </Container>
+      )
+   }
 }
 
-export default Saved;
+export default SavedBooks;
