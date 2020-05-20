9abcddc7e145e198f40cce7c92be3853907e6e8f
Kelvin Almonte <kelvin.almonte95@gmail.com>
2020-05-20 08:50:54 -0400

    Added a button to navigate back to the previous page and organized the CSS code.

--- client/src/pages/NotFound/NotFound.js
+++ client/src/pages/NotFound/NotFound.js
@@ -1,16 +1,27 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 import classes from './NotFound.module.css';
 
-function notFound() {
+function NotFound() {
+  let history = useHistory();
+
+  const redirectHandler = () => history.goBack();
+
   return (
     <main className={classes.NotFoundPage}>
-      <div className={classes.Info}>
+      <div className={classes.InfoDiv}>
         <h1 className={classes.Emoji}>4<span role="img" aria-label="Face With Rolling Eyes Emoji">😔</span>4</h1>
 
         <h1 className={classes.Text}>
           OOPS! PAGE NOT FOUND
-      </h1>
+        </h1>
+
+        <button
+          className={classes.RedirectBtn}
+          onClick={redirectHandler}>
+          Go Back
+        </button>
       </div>
 
       <div className={classes.Background}></div>
@@ -19,4 +30,4 @@ function notFound() {
 }
 // 🙄
 
-export default notFound;
+export default NotFound;
