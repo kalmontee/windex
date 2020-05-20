fda623232aa5409e3eebd5e4d529f87d9569f0bd
Kelvin Almonte <kelvin.almonte95@gmail.com>
2020-05-20 08:49:22 -0400

    Modified the Input Search component and css.

--- client/src/components/InputSearch/InputSearch.js
+++ client/src/components/InputSearch/InputSearch.js
@@ -1,19 +1,18 @@
 import React from 'react';
+
 import classes from './InputSearch.module.css'
 
-const Input = (props) => {
-  return (
-    <form className={classes.FormControl} onSubmit={props.formSubmit}>
-      <input
-        onChange={props.handleInputChange}
-        value={props.value}
-        placeholder="Search for book titles or authors.."
-        name="search"
-        type="text"
-      />
-      <button type="submit">Search</button>
-    </form>
-  )
-}
+const Input = (props) => (
+  <form className={classes.FormControl} onSubmit={props.formSubmit}>
+    <input
+      onChange={props.handleInputChange}
+      value={props.value}
+      placeholder="Search for book titles or authors.."
+      name="search"
+      type="search"
+    />
+    <button type="submit">Search</button>
+  </form>
+)
 
 export default Input;
