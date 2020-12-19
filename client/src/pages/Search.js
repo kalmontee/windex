4ef2a00c51f737ddf382d6c9e4d21a29478995cc
Kelvin Almonte <kelvin.almonte95@gmail.com>
2020-12-19 18:32:24 -0500

    Refractor the code. Changed the class component to functional component and used Hooks.

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,72 +1,46 @@
-import React, { Component } from 'react';
+import React, { useState } from "react";
 
 import { Container, Row } from "../components/Grid";
-import Jumbotron from '../components/UI/Jumbotron/Jumbotron';
-import BookDetails from '../components/Books/BookDetails/BookDetails';
-import BookList from '../components/Books/BookDetails/BookList/BookList';
-import InputSearch from '../components/InputSearch/InputSearch';
-import Spinner from '../components/UI/Spinner/Spinner';
-import Modal from '../components/UI/Modal/Modal';
+import Jumbotron from "../components/UI/Jumbotron/Jumbotron";
+import BookDetails from "../components/Books/BookDetails/BookDetails";
+import BookList from "../components/Books/BookDetails/BookList/BookList";
+import InputSearch from "../components/InputSearch/InputSearch";
+import Spinner from "../components/UI/Spinner/Spinner";
+import Modal from "../components/UI/Modal/Modal";
 import API from "../utils/API";
 
-class SearchBooks extends Component {
-  state = {
-    search: "",
-    message: "",
-    loader: false,
-    showModal: false,
-    books: [],
-  }
+const SearchBooks = () => {
+  const [message, setMessage] = useState("");
+  const [loader, setLoader] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [books, setBooks] = useState([]);
 
-  // To grabbed the value of the user Input
-  handleInputChange = (event) => {
-    const { name, value } = event.target
-    this.setState({ [name]: value });
-  };
-
-  // Closing the Modal once the user clicks anywhere besides the modal
-  modalClosedHanlder = () => this.setState({ showModal: false });
-
-  // OnClick button function to receive all the data
-  handleFormSubmit = (event) => {
-    event.preventDefault();
-    this.searchGoogleBooks();
-  }
-
-  // Making a request to Google Books API
-  searchGoogleBooks = () => {
+  const searchGoogleBooks = (value) => {
     // Initialize the loader
-    this.setState({ loader: true });
+    setLoader(true);
 
-    API.search(this.state.search)
-      .then(res => {
-        this.setState({
-          books: res.data.items,
-          loader: false // remove the loader once it finds results
-        });
+    API.search(value)
+      .then((res) => {
+        setBooks(res.data.items);
+        setLoader(false); // remove the loader once it finds results
       })
-
       // Will output an error.
       .catch(() => {
-        if (this.state.search === "") {
-          return this.setState({
-            message: "Cannot enter an empty search.",
-            showModal: true,
-            modalClosed: false,
-            loader: false
-          })
+        setShowModal(true);
+        if (value === "") {
+          setMessage("Cannot enter an empty search.");
+          setLoader(false);
+          // modalClosed: false,
+        } else {
+          setBooks([]);
+          setLoader(false);
+          setMessage("We apologize.. No books found, try a different search");
         }
-
-        this.setState({
-          books: [],
-          showModal: true,
-          message: "We apolgize.. No Books Found, Try a Different Query"
-        });
       });
-  }
+  };
 
-  saveBookHandler = (id) => {
-    const book = this.state.books.find(book => book.id === id);
+  const saveBookHandler = (id) => {
+    const book = books.find((book) => book.id === id);
 
     const foundResults = {
       id: book.id,
@@ -74,68 +48,76 @@ class SearchBooks extends Component {
       link: book.volumeInfo.infoLink,
       authors: book.volumeInfo.authors,
       description: book.volumeInfo.description,
-      image: book.volumeInfo.imageLinks.thumbnail
-    }
+      image: book.volumeInfo.imageLinks.thumbnail,
+    };
 
     // Saving all books contents to the database
     API.saveBook(foundResults)
-      .then(() => this.setState({ showModal: true, message: "Your book is now saved. Go to your Saved page to view!" }))
-      .catch(() => this.setState({ showModal: true, message: "You already saved this book." }));
+      .then(() => {
+        setShowModal(true);
+        setMessage("Your book is now saved. Go to your Saved page to view!");
+      })
+      .catch(() => {
+        setShowModal(true);
+        setMessage("You already saved this book.");
+      });
+  };
+
+  // Closing the Modal once the user clicks anywhere besides the modal
+  const modalClosedHanlder = () => setShowModal(false);
+
+  let booksResult = null;
+  if (books.length > 0) {
+    booksResult = (
+      <BookList>
+        {books.map((book) => {
+          return (
+            <BookDetails
+              // Getting books with the same IDs. If they have the same ID then give book.etag as a second ID.
+              key={book.id ? book.etag : null}
+              authors={
+                book.volumeInfo.authors
+                  ? book.volumeInfo.authors.join(", ")
+                  : null
+              }
+              title={book.volumeInfo.title}
+              synopsis={
+                book.volumeInfo.description ? book.volumeInfo.description : null
+              }
+              link={book.volumeInfo.infoLink}
+              thumbnail={
+                book.volumeInfo.imageLinks === undefined
+                  ? null
+                  : book.volumeInfo.imageLinks.thumbnail
+              }
+              booksBtnHandler={() => saveBookHandler(book.id)}
+              name="Save Book"
+              btnType={"SaveBtn"}
+            />
+          );
+        })}
+      </BookList>
+    );
   }
 
-  render() {
-    let booksResult = null;
-    if (this.state.books.length > 0) {
-      booksResult = (
-        <BookList>
-          {this.state.books.map(book => {
-            return (
-              <BookDetails
-                // Getting books with the same IDs. If they have the same ID then give book.etag as a second ID.
-                key={book.id ? book.etag : null}
-                authors={book.volumeInfo.authors ? book.volumeInfo.authors.join(", ") : null}
-                title={book.volumeInfo.title}
-                synopsis={book.volumeInfo.description ? book.volumeInfo.description : null}
-                link={book.volumeInfo.infoLink}
-                thumbnail={book.volumeInfo.imageLinks === undefined ? null : book.volumeInfo.imageLinks.thumbnail}
-                booksBtnHandler={() => this.saveBookHandler(book.id)}
-                name="Save Book"
-                btnType={"SaveBtn"}
-              />
-            )
-          })}
-        </BookList>
-      );
-    }
-
-    if (this.state.loader) {
-      booksResult = <Spinner />
-    }
-
-    return (
-      <Container>
-        <Row>
-          <Jumbotron />
-        </Row>
-        <Row>
-          <InputSearch
-            name="query"
-            value={this.state.value}
-            handleInputChange={this.handleInputChange}
-            formSubmit={(event) => this.handleFormSubmit(event)}
-          />
-        </Row>
-        <Modal
-          show={this.state.showModal}
-          modalClosed={this.modalClosedHanlder}>
-          <h3>{this.state.message}</h3>
-        </Modal>
-        <Row>
-          {booksResult}
-        </Row>
-      </Container>
-    )
+  if (loader) {
+    booksResult = <Spinner />;
   }
-}
+
+  return (
+    <Container>
+      <Row>
+        <Jumbotron />
+      </Row>
+      <Row>
+        <InputSearch searchGoogleBooks={searchGoogleBooks} />
+      </Row>
+      <Modal show={showModal} modalClosed={modalClosedHanlder}>
+        <h3>{message}</h3>
+      </Modal>
+      <Row>{booksResult}</Row>
+    </Container>
+  );
+};
 
 export default SearchBooks;
