5b4b9e5e54e5b101cc2fd586cf7b8926a8b5a471
Kelvin Almonte <kelvin.almonte95@gmail.com>
2020-12-19 18:31:15 -0500

    Refractor the code. Used React hooks.

--- client/src/components/InputSearch/InputSearch.js
+++ client/src/components/InputSearch/InputSearch.js
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import classes from './InputSearch.module.css'
+import classes from "./InputSearch.module.css";
 
-const Input = (props) => (
-  <form className={classes.FormControl} onSubmit={props.formSubmit}>
-    <input
-      onChange={props.handleInputChange}
-      value={props.value}
-      placeholder="Search for book titles or authors.."
-      name="search"
-      type="search"
-    />
-    <button type="submit">Search</button>
-  </form>
-)
+const Input = (props) => {
+  const [value, setValue] = useState("");
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    props.searchGoogleBooks(value);
+  };
+
+  return (
+    <form className={classes.FormControl} onSubmit={handleFormSubmit}>
+      <input
+        name="search"
+        type="search"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        placeholder="Search for books or authors.."
+      />
+      <button type="submit">Search</button>
+    </form>
+  );
+};
 
 export default Input;
