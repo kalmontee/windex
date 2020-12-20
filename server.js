c8bfa26dbddaef20a1db1dceb42498180403eb44
Kelvin Almonte <kelvin.almonte95@gmail.com>
2020-12-19 21:34:25 -0500

    Added MongoDB atlas for new db development.

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 5000;
+require("dotenv").config();
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -10,14 +11,19 @@ app.use(express.json());
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
+  app.use(express.static("client/build"));
 }
 
 // Add routes, both API and view
 app.use(routes);
 
-// Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
+// Connect to the MongoDB Atlas or Localhost
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
 
 // Start the API server
-app.listen(PORT, () => console.log(`🌎 ==> API Server now listening on PORT ${PORT}!`));
+app.listen(PORT, () =>
+  console.log(`🌎 ==> API Server now listening on PORT ${PORT}!`)
+);
