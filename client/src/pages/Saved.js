9e3fa05c2ab07e387e5fba99fcf12ff9a9aa5bd7
Kelvin Almonte <kelvin.almonte95@gmail.com>
2020-12-19 21:37:26 -0500

    Changed Component to a functional component using React Hooks.

--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -1,76 +1,84 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from "react";
 
-import { Container, Row } from '../components/Grid/index';
-import Jumbotron from '../components/UI/Jumbotron/Jumbotron';
-import BookDetails from '../components/Books/BookDetails/BookDetails';
-import BookList from '../components/Books/BookDetails/BookList/BookList';
-import Spinner from '../components/UI/Spinner/Spinner';
-import API from '../utils/API';
+import { Container, Row } from "../components/Grid/index";
+import Jumbotron from "../components/UI/Jumbotron/Jumbotron";
+import BookDetails from "../components/Books/BookDetails/BookDetails";
+import BookList from "../components/Books/BookDetails/BookList/BookList";
+import Spinner from "../components/UI/Spinner/Spinner";
+import API from "../utils/API";
+import Modal from "../components/UI/Modal/Modal";
 
-class SavedBooks extends Component {
-   state = {
-      allBooks: [],
-      loader: false,
-      error: ""
+const SavedBooks = () => {
+  const [books, setBooks] = useState([]);
+  const [loader, setLoader] = useState(false);
+  const [error, setError] = useState();
+  const [showModal, setShowModal] = useState(false);
+
+  useEffect(() => {
+    setLoader(true);
+    API.getBooks()
+      .then((res) => {
+        setBooks(res.data);
+        setLoader(false);
+      })
+      .catch(() => setError("Cannot retrieve current data.."));
+  }, []);
+
+  const deleteBookHandler = (id) => {
+    API.deleteBook(id)
+      .then(() => window.location.reload())
+      .catch(() => setError("Cannot delete current book.."));
+  };
+
+  // Closing the Modal once the user clicks anywhere besides the modal
+  const modalClosedHanlder = () => setShowModal(false);
+
+   let booksResult = null;
+   if (books.length > 0) {
+     booksResult = (
+       <BookList>
+         {books.map((book) => {
+           return (
+             <BookDetails
+               key={book._id}
+               authors={book.authors.join(", ")}
+               title={book.title}
+               synopsis={book.description}
+               link={book.link}
+               thumbnail={book.image}
+               booksBtnHandler={() => deleteBookHandler(book._id)}
+               name="Delete Book"
+               btnType={"DeleteBtn"}
+             />
+           );
+         })}
+       </BookList>
+     );
+   } else {
+     booksResult = (
+       <h1 style={{ textAlign: "center", marginTop: "50px" }}>
+         You don't have any books saved.
+       </h1>
+     );
    }
 
-   // componentDidAmount will automatically fetch data from the server
-   componentDidMount = () => {
-      // Initialize the loader
-      this.setState({ loader: true });
-      // Calling API.getBooks function to make a request to the server
-      API.getBooks()
-         .then(res => this.setState({ allBooks: res.data, loader: false }))
-         .catch(err => this.setState({ error: err }));
+   if (loader) {
+     booksResult = <Spinner />;
    }
 
-   deleteBookHandler = (id) => {
-      API.deleteBook(id)
-         .then(res => this.componentDidMount())
-         .catch(err => console.log(err));
-   }
-
-   render() {
-      let booksResult = null;
-      if (this.state.allBooks.length > 0) {
-         booksResult = (
-            <BookList>
-               {this.state.allBooks.map(book => {
-                  return (
-                     <BookDetails
-                        key={book._id}
-                        authors={book.authors.join(', ')}
-                        title={book.title}
-                        synopsis={book.description}
-                        link={book.link}
-                        thumbnail={book.image}
-                        booksBtnHandler={() => this.deleteBookHandler(book._id)}
-                        name="Delete Book"
-                        btnType={"DeleteBtn"}
-                     />
-                  )
-               })}
-            </BookList>
-         );
-      } else {
-         booksResult = <h1 style={{ textAlign: 'center', marginTop: '50px' }}>You don't have any books saved.</h1>
-      }
-
-      if (this.state.loader) {
-         booksResult = <Spinner />
-      }
-
-      return (
-         <Container>
-            <Row>
-               <Jumbotron />
-            </Row>
-            <Row>
-               {booksResult}
-            </Row>
-         </Container>
-      )
-   }
-}
+  return (
+    <Container>
+      <Row>
+        <Jumbotron />
+      </Row>
+      <Modal show={showModal} modalClosed={modalClosedHanlder}>
+        <h3>{error}</h3>
+      </Modal>
+      <Row>
+        {booksResult}
+      </Row>
+    </Container>
+  );
+};
 
 export default SavedBooks;
